3f6affe545c8b3fb8cb6f2c69e999b41e7bc7cf2
Bert Overduin <bert.overduin@aridhia.com>
2025-02-27 09:17:52 +0000

    Added check to dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,3 +1,13 @@
-install.packages("shiny")
-install.packages("DT")
-install.packages("ggplot2")
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i)
+    }
+  }
+}
+
+# Source this script to install all the libraries needed for the app
+
+packages <- c("shiny", "DT", "ggplot2")
